87647e2f8479bd3cc7d9e33362658be0e0995c83
beth182 <bethany.saunders@pgr.reading.ac.uk>
2022-10-27 18:34:39 +0100

    setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # model_eval_tools
 Python tools used for the model evaluation study 
+
+install with pip install -e D:/Documents/model_eval_tools
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='model_eval_tools',
+    url='https://github.com/beth182/model_eval_tools',
+    author='Beth Saunders',
+    author_email='bethany.saunders@pgr.reading.ac.uk',
+    packages=[],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'geopandas',
+        'pandas',
+        'shapely'
+    ],
+    version='0.1',
+    license='MIT',
+    description='todo The description text',
+    long_description='todo The long description text'
+)
